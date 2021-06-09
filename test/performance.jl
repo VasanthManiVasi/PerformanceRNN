17f4a8d230f6f6e6007dc8f0018d823ebfe39cb8
Vasanth <vasanth.mani.vasi.2@gmail.com>
2021-06-10 01:56:09 +0530

    Add tests

--- test/performance.jl
+++ test/performance.jl
@@ -1,25 +1,95 @@
-@testset "PerformanceEvent encoding and decoding" begin
+@testset "PerformanceEvent encoding/decoding" begin
+    pe = PerformanceEvent
     perf = Performance(velocity_bins = 16)
     pairs = [
-        (PerformanceEvent(NOTE_ON, 60), 60),
-        (PerformanceEvent(NOTE_ON, 0), 0),
-        (PerformanceEvent(NOTE_ON, 22), 22),
-        (PerformanceEvent(NOTE_ON, 127), 127),
-        (PerformanceEvent(NOTE_OFF, 72), 200),
-        (PerformanceEvent(NOTE_OFF, 0), 128),
-        (PerformanceEvent(NOTE_OFF, 22), 150),
-        (PerformanceEvent(NOTE_OFF, 127), 255),
-        (PerformanceEvent(TIME_SHIFT, 10), 265),
-        (PerformanceEvent(TIME_SHIFT, 1), 256),
-        (PerformanceEvent(TIME_SHIFT, 72), 327),
-        (PerformanceEvent(TIME_SHIFT, 100), 355),
-        (PerformanceEvent(VELOCITY, 5), 360),
-        (PerformanceEvent(VELOCITY, 1), 356),
-        (PerformanceEvent(VELOCITY, 16), 371)
+        (pe(NOTE_ON, 60), 60),
+        (pe(NOTE_ON, 0), 0),
+        (pe(NOTE_ON, 22), 22),
+        (pe(NOTE_ON, 127), 127),
+        (pe(NOTE_OFF, 72), 200),
+        (pe(NOTE_OFF, 0), 128),
+        (pe(NOTE_OFF, 22), 150),
+        (pe(NOTE_OFF, 127), 255),
+        (pe(TIME_SHIFT, 10), 265),
+        (pe(TIME_SHIFT, 1), 256),
+        (pe(TIME_SHIFT, 72), 327),
+        (pe(TIME_SHIFT, 100), 355),
+        (pe(VELOCITY, 5), 360),
+        (pe(VELOCITY, 1), 356),
+        (pe(VELOCITY, 16), 371)
     ]
 
     for (event, index) in pairs
         @test index == encodeindex(event, perf)
         @test event == decodeindex(index, perf)
+    end
+end
+
+@testset "Performance steps and set_length" begin
+    @testset "Add length" begin
+        perf = Performance()
+        pe = PerformanceEvent
+        set_length(perf, 42)
+        @test perf.numsteps == 42
+        @test perf.events == [pe(TIME_SHIFT, 42)]
+
+        set_length(perf, 142)
+        @test perf.numsteps == 142
+        @test perf.events == [pe(TIME_SHIFT, 100), pe(TIME_SHIFT, 42)]
+        
+        set_length(perf, 200)
+        @test perf.numsteps == 200
+        @test perf.events == [pe(TIME_SHIFT, 100), pe(TIME_SHIFT, 100)]
+    end
+
+    @testset "Remove length" begin
+        perf = Performance()
+        pe = PerformanceEvent
+        events = [
+            pe(NOTE_ON, 60),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 60),
+            pe(NOTE_ON, 64),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 64),
+            pe(NOTE_ON, 67),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 67)
+        ]
+        append!(perf, events)
+        set_length(perf, 200)
+        result_events = [
+            pe(NOTE_ON, 60),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 60),
+            pe(NOTE_ON, 64),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 64),
+            pe(NOTE_ON, 67)
+        ]
+
+        @test perf.events == result_events
+
+        set_length(perf, 50)
+        result_events = [
+            pe(NOTE_ON, 60),
+            pe(TIME_SHIFT, 50)
+        ]
+
+        @test perf.events == result_events
+    end
+
+    @testset "numsteps" begin
+        perf = Performance()
+        pe = PerformanceEvent
+        events = [
+            pe(VELOCITY, 32),
+            pe(NOTE_ON, 60),
+            pe(TIME_SHIFT, 100),
+            pe(NOTE_OFF, 60)
+        ]
+        append!(perf, events)
+
+        @test perf.numsteps == 100
     end
 end

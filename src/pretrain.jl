4622a3f2bf3436358b4d84dad63c11d2ada94008
Vasanth <vasanth.mani.vasi.2@gmail.com>
2021-06-28 22:17:33 +0530

    Consistent indent

--- src/pretrain.jl
+++ src/pretrain.jl
@@ -79,24 +79,23 @@ Load weights from a tensorflow checkpoint file into a Dict.
 readckpt(path) = error("readckpt require TensorFlow.jl installed. run `Pkg.add(\"TensorFlow\"); using TensorFlow`")
 
 @init @require TensorFlow="1d978283-2c37-5f34-9a8e-e9c0ece82495" begin
-  import .TensorFlow
-  #should be changed to use c api once the patch is included
-  function readckpt(path)
-    weights = Dict{String, Array}()
-    TensorFlow.init()
-    ckpt = TensorFlow.pywrap_tensorflow.x.NewCheckpointReader(path)
-    shapes = ckpt.get_variable_to_shape_map()
+    import .TensorFlow
+    function readckpt(path)
+        weights = Dict{String, Array}()
+        TensorFlow.init()
+        ckpt = TensorFlow.pywrap_tensorflow.x.NewCheckpointReader(path)
+        shapes = ckpt.get_variable_to_shape_map()
 
-    for (name, shape) ∈ shapes
-      weight = ckpt.get_tensor(name)
-      if length(shape) == 2 && name != "cls/seq_relationship/output_weights"
-        weight = collect(weight')
-      end
-      weights[name] = weight
+        for (name, shape) ∈ shapes
+            weight = ckpt.get_tensor(name)
+            if length(shape) == 2 && name != "cls/seq_relationship/output_weights"
+                weight = collect(weight')
+            end
+            weights[name] = weight
+        end
+
+        weights
     end
-
-    weights
-  end
 end
 
 """     load_pretrain(path)
